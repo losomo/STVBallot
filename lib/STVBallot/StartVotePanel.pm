402f804da9a28e49b5cb28c1b8ab8561d95a1cb7
Václav Novák <vaclav.novak@captaworks.com>
2012-08-09 17:00:44 +0200

    Scroll Bar OK

--- lib/STVBallot/StartVotePanel.pm
+++ lib/STVBallot/StartVotePanel.pm
@@ -1,7 +1,7 @@
 package STVBallot::StartVotePanel;
 use Modern::Perl;
 use base qw(Wx::Panel);
-use Wx qw(:id wxVERTICAL wxHORIZONTAL wxTE_RIGHT wxTE_PROCESS_ENTER);
+use Wx qw(:id wxVERTICAL wxHORIZONTAL wxTE_RIGHT wxTE_PROCESS_ENTER wxEXPAND);
 use Wx::Event qw(EVT_KILL_FOCUS EVT_TEXT_ENTER);
 use STVBallot::BallotApp qw(lh);
 use Data::Dump;
@@ -51,21 +51,50 @@ sub new {
     my $right_sizer = Wx::BoxSizer->new(wxVERTICAL);
     $right_panel->SetSizer($right_sizer);
 
-    my $candidates_caption = Wx::StaticText->new($right_panel, -1, lh->maketext('Candidates')); 
+    my $candidates_caption = Wx::StaticText->new($right_panel, -1, lh->maketext('Candidates') . ':'); 
     $right_sizer->Add($candidates_caption);
 
-    my $candidates_grid = Wx::Panel->new($right_panel);
+    my $candidates_scroll_pane = Wx::ScrolledWindow->new($right_panel);
+    my $scroll_sizer = Wx::BoxSizer->new(wxVERTICAL);
+    $candidates_scroll_pane->SetSizer($scroll_sizer);
+    $candidates_scroll_pane->SetScrollRate(20, 20);
+    my $candidates_grid = Wx::Panel->new($candidates_scroll_pane);
     my $candidates_sizer = Wx::FlexGridSizer->new(0, 2);
     $candidates_grid->SetSizer($candidates_sizer);
     my $candidates_handler = sub {
         my $no = $this->{candidates_count_input}->GetValue;
-        # TODO create or delete controls
+        my $current_no = @{$this->{candidates_controls} // []};
+        if ($no > $current_no) {
+            for (1..($no-$current_no)) {
+                my $candidate_name_input = Wx::TextCtrl->new($candidates_grid, -1, '');
+                $candidate_name_input->SetMinSize([200, -1]);
+                my $candidate_gender_select = Wx::Choice->new($candidates_grid, -1, [-1,-1], [-1,-1], [
+                    '---',
+                    lh->maketext('female'),
+                    lh->maketext('male'),
+                    ]);
+                $candidates_sizer->Add($candidate_name_input);
+                $candidates_sizer->Add($candidate_gender_select);
+                push @{$this->{candidates_controls}}, [$candidate_name_input, $candidate_gender_select];
+            }
+        }
+        elsif ($no < $current_no) {
+            for (1..($current_no-$no)) {
+                my $controls = pop @{$this->{candidates_controls}};
+                $candidates_sizer->Remove($controls->[1]);
+                $candidates_sizer->Remove($controls->[0]);
+                $controls->[1]->Destroy();
+                $controls->[0]->Destroy();
+            }
+        }
+        $this->Layout();
     };
     EVT_KILL_FOCUS($this->{candidates_count_input}, $candidates_handler);
     EVT_TEXT_ENTER($this->{candidates_count_input}, -1, $candidates_handler);
-    $right_sizer->Add($candidates_grid);
+    $scroll_sizer->Add($candidates_grid, 1, wxEXPAND);
+    $right_sizer->Add($candidates_scroll_pane, 1, wxEXPAND);
     $main_sizer->Add(20, 20);
-    $main_sizer->Add($right_panel);
+    $main_sizer->Add($right_panel, 1, wxEXPAND);
     return $this;
 }
 
