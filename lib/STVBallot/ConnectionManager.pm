98d69186e9f2781ae658d2f0b13213b9a7d5648c
Václav Novák <vaclav.novak@captaworks.com>
2012-08-16 01:22:23 +0200

    rewritten to use Wx::Socket, handshake not ready

--- lib/STVBallot/ConnectionManager.pm
+++ lib/STVBallot/ConnectionManager.pm
@@ -1,16 +1,18 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 package STVBallot::ConnectionManager;
 use Modern::Perl;
 use Data::Dump;
 use STVBallot::BallotApp qw(lh);
-use Wx::Event qw(EVT_COMMAND);
-use JSON; 
+use JSON;
 use Net::Rendezvous::Publish;
 use Net::Bonjour;
 use Encode;
 use Crypt::OpenSSL::RSA;
 use Crypt::CBC;
-use Digest::MD5 qw(md5 md5_hex md5_base64);
+use Digest::MD5 qw(md5_base64);
+use Wx qw(:socket);
+use Wx::Socket;
+use Wx::Event qw(EVT_COMMAND EVT_SOCKET_INPUT EVT_SOCKET_LOST EVT_SOCKET_CONNECTION) ;
 
 use constant SERVICE_PORT => 51432;
 my $DONE_EVENT : shared = Wx::NewEventType;
@@ -38,30 +40,40 @@ sub find_servers_async {
         my $result : shared;
         my @servers = map {
             address => $_->address,
-            name => decode_utf8(($_->all_attrs)[2]),
-            sec_code => ($_->all_attrs)[0]
+            name => decode_utf8($_->txtdata()->[0]),
+            sec_code => $_->txtdata->[1]
         }, $self->_discover_servers();
         $result = encode_json([@servers]);
         my $threvent = new Wx::PlThreadEvent( -1, $DONE_EVENT, $result);
         Wx::PostEvent($widget, $threvent);
-    })->detach();    
+    })->detach();
 }
 
 sub join_session {
     my ($self, $session) = @_;
-    my $sock = IO::Socket::INET->new(
-            PeerAddr => $session->{address},
-            PeerPort => SERVICE_PORT,
-            Proto    => 'tcp');
+    my $app_control = $self->{app_control};
+    $self->{enc_key} = rand;
+    my $sock = Wx::SocketClient->new(wxSOCKET_WAITALL);
+    EVT_SOCKET_LOST($app_control->{app} , $sock , sub {
+        ddx "Connection lost, reconnecting";
+        join_session($session);
+    });
+    $sock->Connect($session->{address}, SERVICE_PORT);
+    if (! $sock->IsConnected ) { ddx "ERROR", $sock;}
     $self->{socket} = $sock;
-    $self->{enc_key} = get_random_bytes(256);
-    $sock->print(encode_json({command => 'get_public_key'}));
-    my $server_key = $sock->getline;
+    $sock->WriteMsg(encode_json({command => 'get_public_key'}) . "\n");
+    ddx "key requested";
+    my $server_key = _readMsg($sock);
+    ddx "key received: $server_key";
     $server_key = Crypt::OpenSSL::RSA->new_public_key($server_key);
     md5_base64($server_key->get_public_key_string) eq $session->{sec_code} or die "security code mismatch";
-    $sock->print(encode_json({command => 'set_enc_key', enc_key => $server_key->encrypt($self->{enc_key})}));
-    my $confirmation = $sock->getline;
+    $sock->WriteMsg(encode_json({command => 'set_enc_key', enc_key => $server_key->encrypt($self->{enc_key})}) . "\n");
+    my $confirmation = _readMsg($sock);
     $confirmation =~ /OK/ or die "Couldn't set encryption key";
+    EVT_SOCKET_INPUT($app_control->{app}, $sock , sub {
+            my ($s , $this , $evt) = @_ ;
+            $self->_client_handle_command(_readMsg($s));
+    }) ;
 }
 
 sub _start_server {
@@ -69,34 +81,16 @@ sub _start_server {
     my $rsa = $self->_generate_key();
     $app_control->{key} = $rsa;
     $app_control->{sec_code} = md5_base64($rsa->get_public_key_string);
-    threads->create(sub {
-        my ($s, $sock);
-        if(!($sock = IO::Socket::INET->new( Listen => 5,
-                           LocalAddr => 'localhost',
-                           LocalPort => SERVICE_PORT,
-                           Proto     => 'tcp',
-                           Reuse     => 1,
-                         )) ) {
-            die "unable to create socket: ", &IO::Socket::INET::errstr, "\n";
-        }
-        while (1) {
-          while(($s = $sock->accept())) {
-              if( ! $s ) {
-                  warn "error: ", $sock->errstr, "\n";
-                  next;
-              }
-              warn "connection opened ($s).\n";
-              my $line = <$s>;
-              while ($line) {
-                  ddx $line;
-                  $line = <$s>;
-              }
-              warn "\t connection closed.\n";
-          }
-        }
-        $sock->close();
-        warn "loop exited.\n";
-    })->detach();
+    my $sock = Wx::SocketServer->new('0.0.0.0',SERVICE_PORT,wxSOCKET_WAITALL);
+    EVT_SOCKET_CONNECTION($app_control->{app}, $sock , sub {
+        my ( $soc , $this , $evt ) = @_ ;
+        my $client = $soc->Accept(0) ;
+        ddx $client;
+        EVT_SOCKET_INPUT($app_control->{app} , $client , sub {
+                my ($s , $this , $evt) = @_ ;
+                $self->_server_handle_command($s, _readMsg($s));
+        });
+    });
 }
 
 sub _publish_server_async {
@@ -123,6 +117,24 @@ sub _discover_servers {
 sub _generate_key {
     my ($self) = @_;
     return Crypt::OpenSSL::RSA->generate_key(1024);
+}
+
+sub _client_handle_command {
+    my ($self, $command) = @_;
+    ddx $command;
+}
+
+sub _server_handle_command {
+    my ($self, $s, $command) = @_;
+    ddx $command;
+    $s->WriteMsg("Whatever\n");
+}
+
+sub _readMsg {
+    my ($sock) = @_;
+    my $buffer;
+    $sock->ReadMsg($buffer, 1024 * 1024 * 1024);
+    return $buffer;
 }
 
 1;
