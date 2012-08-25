d222d52a81073e7a732ade7a533d6a1b5655bc58
Václav Novák <vaclav.novak@captaworks.com>
2012-08-25 12:59:03 +0200

    initial emberjs routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,138 +1,37 @@
-(function() {
-
-// A helper function to define routes for better code reuse
-function tabRoute(name) {
-  return Ember.Route.extend({
-    route: name,
-    connectOutlets: function(router, context) {
-      var TabView = Ember.View.extend({
-        templateName: 'tab' + name
-      });
-      router.get('TabsController').connectOutlet({viewClass: TabView});
-    }
-  });
-}
-
-// A helper function to define a property used to render the navigation. Returns
-// true if a state with the specified name is somewhere along the current route.
-function stateFlag(name) {
-  return Ember.computed(function() {
-    var state = App.router.currentState;
-    while(state) {
-      if(state.name === name) return true;
-      state = state.get('parentState');
-    }
-    return false;
-  }).property('App.router.currentState');
-}
-
-// Create the application
-window.App = Ember.Application.create({
-
-  // Define the main application controller. This is automatically picked up by
-  // the application and initialized.
-  ApplicationController: Ember.Controller.extend({
-  }),
-  ApplicationView: Ember.View.extend({
-    templateName: 'application'
-  }),
-
-  SetupController: Ember.Controller.extend(),
-  SetupView: Ember.View.extend({
-    templateName: 'setup'
-  }),
-
-  TabsController: Ember.Controller.extend({
-    isTabStart: stateFlag('tabStart'),
-    isTabProgress: stateFlag('tabProgress'),
-    isTabEnter: stateFlag('tabEnter')
-  }),
-  TabsView: Ember.View.extend({
-    templateName: 'tabs'
-  }),
-  SetupChoice: Ember.View.extend({
-    tagName: "button",
-    attributeBindings: ['name'],
-    click: function(event){
-        App.mode = name;
-        doTabStart();
-    }
-  }),
-
-  Router: Ember.Router.extend({
-    root: Ember.Route.extend({
-      doSetup: function(router, event) {
-        router.transitionTo('setup');
-      },
-      doTabs: function(router, event, x) {
-        console.log(event, x);
-        router.transitionTo('tabs.index');
-      },
-      setup: Ember.Route.extend({
-        route: '/',
-        connectOutlets: function(router, event) {
-          router.get('applicationController').connectOutlet('setup');
-        }
-      }),
-      tabs: Ember.Route.extend({
-        route: '/tabs',
-        connectOutlets: function(router, event) {
-          router.get('applicationController').connectOutlet('tabs');
-        },
-        index: Ember.Route.extend({
-          route: '/'
-        }),
-        doTabStart: function(router, event) { router.transitionTo('tabs.tabStart'); },
-        tabStart: tabRoute('Start'),
-        doTabProgress: function(router, event) { router.transitionTo('tabs.tabProgress'); },
-        tabProgress: tabRoute('Progress'),
-        doTabEnter: function(router, event) { router.transitionTo('tabs.tabEnter'); },
-        tabEnter: tabRoute('Enter')
-      })
-    })
-  })
-
-});
-    
-
-$(function() {
-App.initialize();
-});
-
-})();
-
-/*
-var App = Em.Application.create();
-
-App.Router = Ember.Router.extend({
-  root: Ember.State.extend({
-    index: Ember.State.extend({
-      route: '/',
-      redirectsTo: 'setup.index'
+var App = Em.Application.create({
+    ApplicationView: Em.View.extend({
+        templateName: 'application'
     }),
-
-    setup: Ember.State.extend({
-      route: '/welcome',
+    ApplicationController: Em.Controller.extend({
     }),
-
-    vote: Ember.State.extend({
-        route: '/vote',
-
-        index: Ember.State.extend({
-          route: '/',
-          redirectsTo: 'vote.start.index'
-        }),
-
-        start: Ember.State.extend({
-          route: '/start',
-        }),
-        progress: Ember.State.extend({
-          route: '/progress',
-        }),
-        enter: Ember.State.extend({
-          route: '/enter',
+    SetupView: Em.View.extend({
+        templateName: 'setup'
+    }),
+    Router: Em.Router.extend({
+        enableLogging: true,
+        root: Em.Route.extend({
+            index: Em.Route.extend({
+                route: '/',
+                redirectsTo: 'modeSetup'
+            }),
+            modeSetup: Em.Route.extend({
+                route: '/modeSetup',
+                connectOutlets: function(router) {
+                    console.log("connecting");
+                    router.get('applicationController').connectOutlet('setup');
+                }
+            }),
+            voteSetup: Em.Route.extend({
+                route: '/voteSetup',
+            }),
+            voteRunning: Em.Route.extend({
+                route: '/voteRunning',
+            }),
+            voteClosed: Em.Route.extend({
+                route: '/voteClosed',
+            })
         })
-    })
-  })
+    }),
 });
-*/
+
+App.initialize();
