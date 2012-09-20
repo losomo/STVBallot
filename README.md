26ec9a8b5659531e62c56b7ec071239326431379
Václav Novák <vaclav.novak@captaworks.com>
2012-09-20 09:35:47 +0200

    perl removed

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ stv-ballot
 ==========
 
 STV ballot desktop LAN application
+
+Standalone Chrome packaged application
+
--- run-stv-ballot.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -Ilib
-use Modern::Perl;
-use STVBallot::BallotApp;
-
-my $app = STVBallot::BallotApp->new();
-$app->MainLoop();
